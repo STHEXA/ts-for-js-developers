0daaa8d52681002aed6522df3dcc5216f01e2de8
STHEXA <ti.syu.s@gmail.com>
2025-08-19 23:25:52 +0900

    literal.tsファイルを追加し、文字列、数値、booleanのリテラル型を定義

---
+++ src/literal.ts
@@ -0,0 +1,17 @@
+// let dayOfWeek: string = "日";
+// dayOfWeek = "月";
+// dayOfWeek = "31";
+
+// 文字列のリテラル型
+let dayOfWeek: "日" | "月" | "火" | "水" | "木" | "金" | "土" = "日";
+dayOfWeek = "月";
+// dayOfWeek = "31";
+
+// 数値のリテラル型
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+month = 6;
+// month = 33
+
+// booleanのリテラル型
+let TRUE: true = true;
+// TRUE = false;
