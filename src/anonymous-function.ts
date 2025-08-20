8ebe74bd685397dda508a4e9e853c650ee6fc291
STHEXA <ti.syu.s@gmail.com>
2025-08-20 22:29:55 +0900

    anonymous-function.tsファイルを追加し、BMI計算の関数を定義

---
+++ src/anonymous-function.ts
@@ -0,0 +1,9 @@
+export {};
+const bmi: (height: number, weight: number) => number = function (
+  height: number,
+  weight: number
+): number {
+  return weight / (height * height);
+};
+
+console.log(bmi(1.62, 64));
