043f0fec46636b75f556e9b988ecc9adcd565000
STHEXA <ti.syu.s@gmail.com>
2025-08-23 16:11:10 +0900

    as-const.tsファイルを追加し、constアサーションの使用例を定義

---
+++ src/as-const.ts
@@ -0,0 +1,15 @@
+export {};
+
+let name = "taishu";
+name = "tai";
+
+let nickname = "ham" as const;
+nickname = "ham";
+
+let profile = {
+  name: "taishu",
+  height: 162,
+} as const;
+
+// profile.name = "taro"
+// profile.height = 165
